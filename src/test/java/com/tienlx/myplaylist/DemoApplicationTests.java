73ea0b61ef79b0a3171b84aa7482fd2c7a2595a1
tienlx93 <tienlxse60897@fpt.edu.vn>
2016-10-07 22:51:37 +0700

    Add file

--- src/test/java/com/tienlx/myplaylist/DemoApplicationTests.java
+++ src/test/java/com/tienlx/myplaylist/DemoApplicationTests.java
@@ -1,16 +1,5 @@
 package com.tienlx.myplaylist;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class DemoApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
 
 }
