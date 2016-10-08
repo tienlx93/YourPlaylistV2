9148a9e3739a01da63969256bfc0f3f5ccbae4af
tienlx93 <tienlxse60897@fpt.edu.vn>
2016-10-08 21:14:22 +0700

    Fix context path

--- src/main/java/com/tienlx/myplaylist/controller/CrawlerController.java
+++ src/main/java/com/tienlx/myplaylist/controller/CrawlerController.java
@@ -40,8 +40,7 @@ public class CrawlerController extends HttpServlet {
         String url = request.getParameter("url");
         String full = request.getParameter("full");
         CrawlerV2 crawler = new CrawlerV2();
-        ServletContext context = request.getSession().getServletContext();
-        String absoluteDiskPath = context.getRealPath("/");
+        String absoluteDiskPath = this.getClass().getResource("/").getPath();
 //        String absoluteDiskPath = getServletContext().getRealPath("/") + File.separator;
         Logger.getLogger(CrawlerV2.class.getName()).log(Level.INFO, "absoluteDiskPath: " + absoluteDiskPath);
         System.out.printf("absoluteDiskPath: " + absoluteDiskPath);
