746d7ed9250ce2291e4cf7075df2e57419aa90be
tienlx93 <tienlxse60897@fpt.edu.vn>
2016-10-08 19:50:33 +0700

    Done phase 1

--- src/main/java/com/tienlx/myplaylist/crawler/HTTPRequest.java
+++ src/main/java/com/tienlx/myplaylist/crawler/HTTPRequest.java
@@ -1,17 +1,14 @@
 package com.tienlx.myplaylist.crawler;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.*;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.params.HttpClientParams;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedReader;
 import java.io.IOException;
-
-import org.apache.commons.httpclient.Header;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.UsernamePasswordCredentials;
-import org.apache.commons.httpclient.auth.AuthScope;
-import org.apache.commons.httpclient.methods.GetMethod;
-
-import java.io.InputStream;
-import java.net.URL;
+import java.io.InputStreamReader;
 
 /**
  * Created by ASUS on 10/08/2016.
@@ -20,42 +17,23 @@ public class HTTPRequest {
     public static String getHTML(String url) throws Exception {
 
         //create a singular HttpClient object
-        HttpClient client = new HttpClient();
+        HttpClient client = HttpClientBuilder.create().build();
 
-        //establish a connection within 5 seconds
-        client.getHttpConnectionManager().
-                getParams().setConnectionTimeout(5000);
 
-        HttpMethod method = null;
+        HttpGet request = new HttpGet(url);
 
-        //create a method object
-        method = new GetMethod(url);
-        method.setFollowRedirects(true);
+        HttpResponse response = client.execute(request);
+        System.out.println("Response Code : "
+                + response.getStatusLine().getStatusCode());
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
 
-        //execute the method
-        String responseBody = null;
-        try{
-            client.executeMethod(method);
-            responseBody = method.getResponseBodyAsString();
-        } catch (HttpException he) {
-            System.err.println("Http error connecting to '" + url + "'");
-            System.err.println(he.getMessage());
-            return null;
-        } catch (IOException ioe){
-            System.err.println("Unable to connect to '" + url + "'");
-            return null;
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
         }
 
-        //write out the response headers
-        System.out.println("*** Response ***");
-        System.out.println("Status Line: " + method.getStatusLine());
-
-        //write out the response body
-        System.out.println("*** Response Body ***");
-        System.out.println(responseBody);
-
-        //clean up the connection resources
-        method.releaseConnection();
-        return responseBody;
+        return result.toString();
     }
 }
