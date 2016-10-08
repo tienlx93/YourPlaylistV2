ab8fe4afb29b4665521f9f6c1aafcdd85e8758bf
tienlx93 <tienlxse60897@fpt.edu.vn>
2016-10-08 22:44:13 +0700

    add deploy hook

--- src/main/java/com/tienlx/myplaylist/controller/CrawlerController.java
+++ src/main/java/com/tienlx/myplaylist/controller/CrawlerController.java
@@ -42,7 +42,6 @@ public class CrawlerController extends HttpServlet {
         CrawlerV2 crawler = new CrawlerV2();
 //        String absoluteDiskPath = this.getClass().getResource("/").getPath();
         String absoluteDiskPath = getServletContext().getRealPath("/") + File.separator;
-        absoluteDiskPath = absoluteDiskPath.substring(0, absoluteDiskPath.lastIndexOf("_")) + File.separator;
         Logger.getLogger(CrawlerV2.class.getName()).log(Level.INFO, "absoluteDiskPath: " + absoluteDiskPath);
         System.out.printf("absoluteDiskPath: " + absoluteDiskPath);
         crawler.setBasePath(absoluteDiskPath);
