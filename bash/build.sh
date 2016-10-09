6ef6a52f824f056641e86141aec276cd2751c576
tienlx93 <tienlxse60897@fpt.edu.vn>
2016-10-09 13:27:21 +0700

    add build script

--- bash/build.sh
+++ bash/build.sh
@@ -2,6 +2,6 @@
 location=$1
 tomcat=$2
 cd ${location}
-sudo cp -rf yourplaylist-1.0/* ${tomcat}
+sudo cp -rf yourplaylist-1.0/* ${tomcat}/ROOT
 sudo chown -R tomcat:tomcat ${tomcat}/ROOT
 sudo service tomcat8 restart
